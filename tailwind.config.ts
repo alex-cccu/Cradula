61a304fb3a04d1883093093e94ece1fec13e23cd
Alex <alex.smith.cccu@gmail.com>
2025-10-14 18:25:39 +0100

    Update body font to quicksand

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                cormorantGaramond: ["var(--font-cormorant-garamond)", "serif"],
-                poppins: ["var(--font-poppins)", "sans-serif"],
-            },
-        }
-    },
-    plugins: [],
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [],
+};
 
-export default config
+export default config;
