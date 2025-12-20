1529c481cce77b26abe25abd31d35ae595d7d0fc
Alex <alex.smith.cccu@gmail.com>
2025-12-20 15:46:32 +0000

    Refactor MagicCardPanel to use input as key in map function

--- components/MagicTheGathering/MagicCardPanel.tsx
+++ components/MagicTheGathering/MagicCardPanel.tsx
@@ -3,8 +3,8 @@ import MagicCardImage from "./MagicCardImage";
 const MagicCardPanel = ({ searchInputs }: { searchInputs: string[] }) => {
   return (
     <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-      {searchInputs.map((input, index) => (
-        <MagicCardImage key={index} searchInput={input} />
+      {searchInputs.map((input) => (
+        <MagicCardImage key={input} searchInput={input} />
       ))}
     </div>
   );
